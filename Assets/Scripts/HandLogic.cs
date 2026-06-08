d5f952691b4392ffbe52801dade3cbbd0f40b6b3
wgreine2 <wgreine2@uwyo.edu>
2026-06-08 16:50:46 -0600

    Hand moves and camera works. Tomorrow will be making pickup work! :)

--- Assets/Scripts/HandLogic.cs
+++ Assets/Scripts/HandLogic.cs
@@ -2,28 +2,69 @@ using UnityEngine;
 
 public class HandLogic : MonoBehaviour
 {
+
+    [Header("Hand Settings")]
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float distanceFromCamera = 5f;
+    public Camera camera;    
+    
+    [Space]
+    [Header("Camera Rotation Settings")]
+    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float rotationBarSize = 0.2f;
+    public GameObject cameraEmpty;
+    Vector2 mousePercent;
+    [SerializeField] private float rotationAmount = 50f;
+    private float currentY = 0f;
 
-    public Camera camera;
+    [Space]
+    [Header("Grab Settings")]
+    [SerializeField] private float grabDistance = 3f;
+    [SerializeField] private Transform grabPoint;
+    [SerializeField] private LayerMask layerToIgnore;
+    private Rigidbody heldObject;
+    private Rigidbody targetObject;
 
     void Update()
     {
+        
+        targetObject = null;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
         Plane plane = new Plane(-camera.transform.forward,camera.transform.position + camera.transform.forward * distanceFromCamera);
-
         float enter;
 
+        //works yay
         if (plane.Raycast(ray, out enter))
         {
             Vector3 targetPosition = ray.GetPoint(enter);
+            transform.position = Vector3.MoveTowards(transform.position,targetPosition,moveSpeed * Time.deltaTime);
+        }
+        //fDNIAJSHDKJASBNKD
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, ~layerToIgnore))
+        {
+            Debug.DrawRay(ray.origin,ray.direction * 100f,Color.red);
+            if (hit.collider.CompareTag("Grabbable"))
+            {
+                Debug.Log("Hit target!");
+            }   
+        }
 
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                targetPosition,
-                moveSpeed * Time.deltaTime
-            );
+        mousePercent = camera.ScreenToViewportPoint(Input.mousePosition);
+        if (mousePercent.x > 1f - rotationBarSize)
+        {
+            currentY += rotationSpeed * Time.deltaTime;
+            currentY = Mathf.Clamp(currentY, -rotationAmount, rotationAmount);
+            cameraEmpty.transform.localRotation = Quaternion.Euler(0f, currentY, 0f);
+            Debug.Log("Mouse is on the right side of the screen!");
+        }
+        else if (mousePercent.x < rotationBarSize)
+        {
+            currentY -= rotationSpeed * Time.deltaTime;
+            currentY = Mathf.Clamp(currentY, -rotationAmount, rotationAmount);
+            cameraEmpty.transform.localRotation =
+            Quaternion.Euler(0f, currentY, 0f);
+            Debug.Log("Mouse is on the left side of the screen!");
         }
     }
 }
