59045464b3257f3c75d3bf0a36cba2c4b121127e
Dani-Hersh <drhershey04@gmail.com>
2026-06-26 16:48:09 -0600

    Cel shading is back and broken, there are more colliders somewhere

---
+++ Assets/Shaders/Cel Shading/celshading.hlsl
@@ -0,0 +1,12 @@
+// #ifindef LIGHTING_CEL_SHADED_INCLUDED
+// #define LIGHTING_CEL_SHADED_INCLUDED
+
+// void LightingCelShaded_float(out float3 Color) {
+// #if defined(SHADERGRAPH_PREVIEW)
+//     ...
+// #else
+//     Color = float3(0.5f,0.5f,0.5f);
+// #endif
+// }
+
+// #endif
